3131b04f517477643f55d77832367e5e38d2f615
[SW5] 문수민 <sum95771@gmail.com>
2023-12-15 19:06:10 +0900

    feat: 초기 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/routing";
+
+// import { ThemeProvider } from "styled-components";
+// import theme from "./styles/theme";
+
+// import GlobalStyles from "./styles/global";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
