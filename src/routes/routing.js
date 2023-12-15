09a3c0ec4ebe7f6f5a93f2f9453982ea1a929eb5
[SW5] 문수민 <sum95771@gmail.com>
2023-12-16 03:21:42 +0900

    feat: 마이페이지 라우터 설정

--- src/routes/routing.js
+++ src/routes/routing.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "Layout";
 import Home from "pages/Home";
+import Mypage from "pages/Mypage";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "/mypage",
+        element: <Mypage />,
       },
     ],
   },
