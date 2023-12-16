ae23e066e90fdfc0f966d4a5fca80613f390f634
[SW5] 문수민 <sum95771@gmail.com>
2023-12-17 01:50:37 +0900

    feat: 일기 데이터 생성, 수정, 삭제 로직 생성 및 분리

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,70 @@
+import { useReducer, useRef } from "react";
+
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
 import * as S from "./index.styled";
 
+const reducer = (state, action) => {
+  let newState = [];
+
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      const newItem = { ...action.data };
+      newState = [newItem, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      const newItem = { ...action.data };
+      newState = state.map((it) => (it.id === action.data.id ? newItem : it));
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
 function Layout() {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0); //초기화 방지
+
+  const onCreate = (date, title, content, mood) => {
+    const newItem = {
+      id: dataId.current,
+      date: newDate(date).getTime(),
+      title,
+      content,
+      mood,
+    };
+    dispatch({ type: "CREATE", data: newItem });
+    dataId.current += 1;
+  };
+
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+    dataId.current += 1;
+  };
+
+  const onEdit = (targetId, date, title, content, mood) => {
+    const newItem = {
+      id: targetId,
+      date: newDate(date).getTime(),
+      title,
+      content,
+      mood,
+    };
+    dispatch({ type: "EDIT", data: newItem });
+  };
+
   return (
     <S.Container>
       <Header />
