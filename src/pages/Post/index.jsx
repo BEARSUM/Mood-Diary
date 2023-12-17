03740546e12bdc10a693001c1b29b7003158a613
[SW5] 문수민 <sum95771@gmail.com>
2023-12-17 14:01:54 +0900

    feat: 일기 쓰기 페이지 추가 및 라우팅

---
+++ src/pages/Post/index.jsx
@@ -0,0 +1,12 @@
+import DiaryEditor from "../../components/common/DiaryEditor";
+import * as S from "./index.styled";
+
+const Post = () => {
+  return (
+    <S.Container>
+      <DiaryEditor />
+    </S.Container>
+  );
+};
+
+export default Post;
---
+++ src/pages/Post/index.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 54px 56px;
+  margin: 0 64px;
+`;
+
+export { Container };
