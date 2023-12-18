5599fe2bfc944c84dc0bb652ab6877b53029d429
[SW5] 문수민 <sum95771@gmail.com>
2023-12-19 01:54:53 +0900

    feat: 마이페이지 일기 데이터 바인딩

--- src/pages/Mypage/index.jsx
+++ src/pages/Mypage/index.jsx
@@ -1,41 +1,22 @@
+import { useRecoilValue } from "recoil";
+import { diaryState } from "recoil/atoms/diary.atom";
+
 import PostItem from "./PostItem";
+
 import * as S from "./index.styled";
 
 const Mypage = () => {
-  //임시 데이터
-  const dummyDiaryList = [
-    {
-      id: 1,
-      title: "떡볶이는 맛있어",
-      content: "달달하고 매콤~한 맛있는 떡볶이.",
-      mood: 5,
-      createdAt: new Date().getTime(),
-    },
-    {
-      id: 2,
-      title: "라면은 맛있어",
-      content: "달달하고 매콤~한 맛있는 라면은",
-      mood: 3,
-      createdAt: new Date().getTime(),
-    },
-    {
-      id: 3,
-      title: "치킨는 맛있어",
-      content: "달달하고 매콤~한 맛있는 치킨.",
-      mood: 2,
-      createdAt: new Date().getTime(),
-    },
-  ];
+  const diary = useRecoilValue(diaryState);
   return (
     <S.Container>
       <S.Header>
         <div>일기 관리</div>
         <S.Number>
-          총 <span>{dummyDiaryList.length}</span>개의 일기
+          총 <span>{diary.length}</span>개의 일기
         </S.Number>
       </S.Header>
       <S.PostList>
-        {dummyDiaryList.map((el) => (
+        {diary.map((el) => (
           <PostItem key={el.id} diary={el} />
         ))}
       </S.PostList>
