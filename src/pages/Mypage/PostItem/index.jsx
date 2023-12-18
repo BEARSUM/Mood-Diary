f5439cd7285c8d288461775fcdd45eb1d0c22f26
[SW5] 문수민 <sum95771@gmail.com>
2023-12-19 02:01:59 +0900

    feat: PostItem 퍼블리싱 수정

--- src/pages/Mypage/PostItem/index.jsx
+++ src/pages/Mypage/PostItem/index.jsx
@@ -6,17 +6,17 @@ const PostItem = ({ diary }) => {
 
   return (
     <S.Container>
-      <S.Row>
-        <S.Mood>{mood}</S.Mood>
-        <S.Date>{createdAt}</S.Date>
-      </S.Row>
-      <S.Row>
-        <S.Title>{title}</S.Title>
-        <S.Buttons>
-          <Button color="reverse">수정</Button>
-          <Button color="error">삭제</Button>
-        </S.Buttons>
-      </S.Row>
+      <S.Col>
+        <img src={`/assets/img/emotion${mood}.png`} alt="mood" />
+        <S.Diary>
+          <S.Title>{title}</S.Title>
+          <S.Date>{createdAt}</S.Date>
+        </S.Diary>
+      </S.Col>
+      <S.Buttons>
+        <Button color="reverse">수정</Button>
+        <Button color="cancel">삭제</Button>
+      </S.Buttons>
     </S.Container>
   );
 };
--- src/pages/Mypage/PostItem/index.styled.js
+++ src/pages/Mypage/PostItem/index.styled.js
@@ -1,34 +1,43 @@
 import styled from "styled-components";
-import { flexColumnCenter, flexSpaceBetweenCenter } from "styles/common";
+import { flexSpaceBetweenCenter } from "styles/common";
 
 const Container = styled.div`
-  padding: 24px 16px;
-  ${flexColumnCenter}
+  ${flexSpaceBetweenCenter}
   gap: 16px;
-
+  padding: 24px 16px;
   border-bottom: 1px solid ${({ theme }) => theme.PALETTE.gray[100]};
 `;
-const Row = styled.div`
-  width: 100%;
-  ${flexSpaceBetweenCenter}
+
+const Diary = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 `;
-const Mood = styled.div`
-  font-weight: 400;
-  font-size: ${({ theme }) => theme.FONT_SIZE.xs};
-  color: ${({ theme }) => theme.PALETTE.mainColor};
+
+const Col = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+
+  img {
+    width: 42px;
+  }
 `;
+
 const Date = styled.div`
   font-weight: 400;
   font-size: ${({ theme }) => theme.FONT_SIZE.xs};
   color: ${({ theme }) => theme.PALETTE.gray[200]};
 `;
+
 const Title = styled.div`
   font-weight: 500;
   font-size: ${({ theme }) => theme.FONT_SIZE.md};
 `;
+
 const Buttons = styled.div`
   display: flex;
   gap: 8px;
 `;
 
-export { Container, Row, Mood, Date, Title, Buttons };
+export { Container, Diary, Col, Date, Title, Buttons };
