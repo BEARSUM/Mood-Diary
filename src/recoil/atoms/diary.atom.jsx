379dff81637e6250994be27958c85685251386a8
[SW5] 문수민 <sum95771@gmail.com>
2023-12-22 01:15:48 +0900

    feat: 일기 데이터 localStorage에 저장

--- src/recoil/atoms/diary.atom.jsx
+++ src/recoil/atoms/diary.atom.jsx
@@ -1,11 +1,16 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const diaryState = atom({
   key: "diaryState",
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const diaryIdState = atom({
   key: "diaryIdState",
-  default: 0,
+  default: 1,
+  effects_UNSTABLE: [persistAtom],
 });
